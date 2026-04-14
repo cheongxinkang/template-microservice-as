58da242439c71306ae09ec52b0a25765c707fdbe
Rtemise <rtemise@gmail.com>
2026-04-14 10:10:31 +0800

    Initialize

---
+++ src/main/java/com/xk/template_service_as/TemplateServiceAsApplication.java
@@ -0,0 +1,13 @@
+package com.xk.template_service_as;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateServiceAsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateServiceAsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xk/template_service_as/TemplateServiceAsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xk.template_service_as;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateServiceAsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
