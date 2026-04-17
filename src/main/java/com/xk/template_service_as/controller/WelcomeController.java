42fea68cd9168e3ba894fbd6a152ec54c180ba2b
Rtemise <rtemise@gmail.com>
2026-04-17 14:48:52 +0800

    Added sample welcome pages from spring pet clinic

---
+++ src/main/java/com/xk/template_service_as/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.xk.template_service_as.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "welcome";
+    }
+
+}
